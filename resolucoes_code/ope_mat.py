212def049cd889beb1b76282e8cfa6cea47130ba
Geo <135927652+geovannapires@users.noreply.github.com>
2025-09-06 23:08:02 -0300

    Update ope_mat.py

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,10 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+um1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+print("A soma dos números é: ", num1 + num2)
+print("A subtração dos números é: ", num1 - num2)
+print("A multiplicação dos números é: ", num1 * num2)
+print("A divisão dos números é: ", num1 / num2)
+
