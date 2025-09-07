4b460138a154fa41d514d895d405768a01d175f7
Geo <135927652+geovannapires@users.noreply.github.com>
2025-09-06 23:11:03 -0300

    Create par_impar.py

---
+++ resolucoes_code/par_impar.py
@@ -0,0 +1,7 @@
+#Como entrada, receba um número inteiro e verifique se ele é par ou ímpar. Uma dica é: Utilize condicionais para realizar a verificação
+
+num = int(input("Digite um número inteiro: "))
+if num % 2 == 0:
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
